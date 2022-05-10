8db4ec3e52872baaf0e1e04c2fa934d3da6bc835
禺狨 <18119604035@163.com>
2022-05-10 18:00:58 +0800

    证书二维码内容校验必须是网址

--- src/views/certificate/create.vue
+++ src/views/certificate/create.vue
@@ -535,18 +535,6 @@ export default {
     createQrcode(val) {
       this.qrcodeStatus = val;
     },
-    validURL(str) {
-      var pattern = new RegExp(
-        "^(https?:\\/\\/)?" +
-          "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" +
-          "((\\d{1,3}\\.){3}\\d{1,3}))" +
-          "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" +
-          "(\\?[;&a-z\\d%_.~+=-]*)?" +
-          "(\\#[-a-z\\d_]*)?$",
-        "i"
-      );
-      return !!pattern.test(str);
-    },
     confirm() {
       if (this.loading) {
         return;
