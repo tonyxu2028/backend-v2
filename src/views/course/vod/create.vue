2e5e7431ed14db908ee6a95649e9f4b500c6ff15
禺狨 <18119604035@163.com>
2022-05-31 15:29:11 +0800

    课程未设置免费时价格应该大于0

--- src/views/course/vod/create.vue
+++ src/views/course/vod/create.vue
@@ -309,7 +309,7 @@ export default {
       if (this.loading) {
         return;
       }
-      if (this.course.is_free === 0 && this.course.charge === 0) {
+      if (this.course.is_free === 0 && parseInt(this.course.charge) === 0) {
         this.$message.error("课程未设置免费时价格应该大于0");
         return;
       }
