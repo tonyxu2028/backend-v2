8aa7677bbab59f58fd2bee033dec5c04a30411c2
禺狨 <18119604035@163.com>
2022-08-22 17:37:44 +0800

    退款弹窗的提示改为通用提示样式

--- src/views/order/index.vue
+++ src/views/order/index.vue
@@ -212,12 +212,9 @@
     <el-dialog title="退款" :visible.sync="visible" width="420px">
       <div class="j-flex flex-column">
         <div class="d-flex">
-          <el-alert
-            title="退款成功不会自动取消已绑定的课程和vip关系"
-            type="warning"
-            :closable="false"
-          >
-          </el-alert>
+          <helper-text
+            text="退款成功不会自动取消课程/会员绑定关系，需手动操作。"
+          ></helper-text>
         </div>
         <div class="d-flex mt-20">
           <label class="mr-20"><span class="c-red mr-5">*</span>退款方式</label>
