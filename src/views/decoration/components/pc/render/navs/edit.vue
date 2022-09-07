80b581b54a862687c58b51cab29506a7b08ff35a
禺狨 <18119604035@163.com>
2022-09-07 10:19:42 +0800

    添加、编辑幻灯片、导航排序值提示文本统一优化

--- src/views/decoration/components/pc/render/navs/edit.vue
+++ src/views/decoration/components/pc/render/navs/edit.vue
@@ -28,7 +28,7 @@
               <template slot="label">
                 <form-label
                   text="排序值"
-                  helper="请填写整数，小数靠前，大数靠后"
+                  helper="填写整数，数字越小排序越靠前"
                 ></form-label>
               </template>
               <el-input v-model="form.sort" class="w-200px"></el-input>
