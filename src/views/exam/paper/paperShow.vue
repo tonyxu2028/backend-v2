ea9016eb6e8a7d2ea989cce7184ba33ab5e42792
禺狨 <18119604035@163.com>
2022-09-28 14:02:39 +0800

    后台试卷查看优化

--- src/views/exam/paper/paperShow.vue
+++ src/views/exam/paper/paperShow.vue
@@ -172,55 +172,8 @@ export default {
           for (let key in normaldata) {
             box.push(...normaldata[key]);
           }
-          let params = [];
-          let choice = [];
-          let select = [];
-          let input = [];
-          let qa = [];
-          let judge = [];
-          let cap = [];
-          box.forEach((item) => {
-            if (item.question) {
-              if (item.question.type === 1) {
-                choice.push(item);
-              }
-              if (item.question.type === 2) {
-                select.push(item);
-              }
-              if (item.question.type === 3) {
-                input.push(item);
-              }
-              if (item.question.type === 4) {
-                qa.push(item);
-              }
-              if (item.question.type === 5) {
-                judge.push(item);
-              }
-              if (item.question.type === 6) {
-                cap.push(item);
-              }
-            }
-          });
 
-          if (choice.length > 0) {
-            params.push(...choice);
-          }
-          if (select.length > 0) {
-            params.push(...select);
-          }
-          if (input.length > 0) {
-            params.push(...input);
-          }
-          if (qa.length > 0) {
-            params.push(...qa);
-          }
-          if (judge.length > 0) {
-            params.push(...judge);
-          }
-          if (cap.length > 0) {
-            params.push(...cap);
-          }
-          this.questions = params;
+          this.questions = box;
         })
         .catch((e) => {
           this.$message.error(e.message);
