5fa5c9af94efa50f562ae7393bb01424446169fa
unknown <18119604035@163.com>
2023-07-24 16:09:22 +0800

    问答优化

--- src/views/wenda/question/category/index.vue
+++ src/views/wenda/question/category/index.vue
@@ -3,7 +3,7 @@
     <back-bar class="mb-30" title="问题分类"></back-bar>
     <div class="float-left mb-30">
       <p-button
-        text="添加"
+        text="新建分类"
         p="addons.Wenda.category.store"
         @click="
           $router.push({
