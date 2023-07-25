bdbdf9ba6c7a60d822ab7631ebc3b7965219196a
unknown <18119604035@163.com>
2023-07-25 09:56:34 +0800

    录播课优化

--- src/views/course/vod/video/comments.vue
+++ src/views/course/vod/video/comments.vue
@@ -12,31 +12,6 @@
       </div>
       <div class="d-flex">
         <div>
-          <el-input
-            class="w-150px"
-            v-model="filter.user_id"
-            placeholder="学员ID"
-          ></el-input>
-        </div>
-        <div class="ml-10">
-          <el-select
-            filterable
-            :filter-method="dataFilter"
-            placeholder="课时"
-            class="w-150px"
-            v-model="filter.video_id"
-            v-el-select-loadmore="loadmore"
-          >
-            <el-option
-              v-for="(item, index) in filterData.videos"
-              :key="index"
-              :label="item.title"
-              :value="item.id"
-            >
-            </el-option>
-          </el-select>
-        </div>
-        <div class="ml-10">
           <el-date-picker
             :picker-options="pickerOptions"
             v-model="filter.created_at"
@@ -64,8 +39,6 @@
       v-loading="loading"
     >
       <el-table-column type="selection" width="55"></el-table-column>
-      <el-table-column prop="user_id" label="学员ID" width="120">
-      </el-table-column>
       <el-table-column label="学员" width="300">
         <template slot-scope="scope">
           <div class="user-item d-flex" v-if="users[scope.row.user_id]">
